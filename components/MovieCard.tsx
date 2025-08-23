bc4608d26f0f60d498dd9142b63d72076a6fa7f9
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-08-22 21:29:49 -0700

    implement MovieCard to render movie poster and title

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,9 +1,28 @@
-import { Link } from 'expo-router'
-import React from 'react'
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
     return (
-        <Link href={`movie/${id}`} asChild></Link>
+        <Link href={`movie/${id}`} asChild>
+            <TouchableOpacity className="w-[30%]">
+                <Image
+                    source={{
+                        uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                    }}
+                    className='w-full h-52 rounded-lg'
+                    resizeMode='cover'
+                />
+                <Text className='text-sm font-bold text-white mt-2'>{title}</Text>
+
+                <View className="flex-row items-center justify-start gap-x-1">
+                    <Image source={icons.star} className='size-4' />
+                    <Text>{Math.round(vote_average / 2)}</Text>
+                </View>
+            </TouchableOpacity>
+        </Link>
     )
 }
 
