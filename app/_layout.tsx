7cc0d8e1de01ee2979d35e4cd345b4208ba3f654
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-09-07 20:58:29 -0700

    style search bar and trending movie

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)"
-      options={{ headerShown: false }} />
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)"
+          options={{ headerShown: false }} />
 
-    <Stack.Screen name="movie/[id]"
-      options={{ headerShown: false }} />
-  </Stack>;
+        <Stack.Screen name="movie/[id]"
+          options={{ headerShown: false }} />
+      </Stack>
+    </>);
 }
