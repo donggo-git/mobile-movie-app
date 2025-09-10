130da953b1b1c8b7a2c55389c93cd95c8a9abd49
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-09-10 00:36:53 -0700

    fix appwrite and render movie detail for poster, vote, title

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,16 +1,34 @@
+import { icons } from '@/constants/icons';
+import { fetchMovieDetail } from '@/services/api';
+import useFetch from '@/services/useFetch';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const MovieDetail = () => {
     const { id } = useLocalSearchParams();
+    const { data: movie, loading } = useFetch(() => fetchMovieDetail(id as string))
     return (
         <View className='bg-primary flex-1'>
             <ScrollView contentContainerStyle={{
                 paddingBottom: 80
             }}>
                 <View>
-                    <Image source={{ uri: `https://image.tmbd.org/t/p/w500${movie ? poster_path}` }} />
+                    <Image source={{
+                        uri: movie?.poster_path ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}` : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                    }} className='w-full h-[550px]' resizeMode='stretch' />
+                </View>
+                <View className='flex-col items-start justify-center mt-5 px-5'>
+                    <Text className='text-white font-bold text-xl'>{movie?.title}</Text>
+                    <View className='flex-row items-center gap-x-1 mt-2'>
+                        <Text className='text-light-200 text-sm'>{movie?.release_date?.split('-')[0]}</Text>
+                        <Text className='text-light-200 text-sm'>{movie?.runtime}m</Text>
+                    </View>
+                    <View className='flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2'>
+                        <Image source={icons.star} className='size-4' />
+                        <Text className='text-white font-bold text-sm'>{Math.round(movie?.vote_average ?? 0)}/10</Text>
+                        <Text className='text-light-200 text-sm'>{movie?.vote_count} votes</Text>
+                    </View>
                 </View>
             </ScrollView>
         </View >
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,9 +1,11 @@
 //track search by user
 import { Client, Databases, ID, Query } from "react-native-appwrite";
-const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
-const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+const DATABASE_ID: string = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID: string = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+const ENDPOINT: string = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!;
+const PROJECT_ID: string = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
 
-const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
 const database = new Databases(client)
 export const updateSearchCount = async (query: string, movie: Movie) => {
     try {
